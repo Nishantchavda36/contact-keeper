7fbb3b6362945b853936bae6c95925d12a590f6a
Nishant Chavda <nishantchavda36@gmail.com>
2023-04-05 18:10:21 -0400

    express validation & hasing password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../modals/User')
@@ -10,6 +11,7 @@ const User = require('../modals/User')
 
 router.post(
   '/',
+
   [
     check('name', 'Please add name').not().isEmpty(),
     check('email', 'Please input valid email').isEmail(),
@@ -18,12 +20,30 @@ router.post(
       'Please enter valid password of minium 6 characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
-    const errors = validationResult(req.body)
+  async (req, res) => {
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-    res.send('passed')
+    const { name, email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (user) {
+        res.status(400).json('User already existed')
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      })
+      let salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(password, salt)
+      await user.save()
+      res.send('User saved')
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send('Server Error')
+    }
   }
 )
 
